e97d58af30ee4f9861e4489f97dd3cb9d6bda7ba
Utkarsh Thakur <thakurutkarsh22@gmail.com>
2026-02-11 20:57:09 +0530

    Session 6 code

---
+++ Session6/index.js
@@ -0,0 +1,11 @@
+// debugging 
+// 1. quesiton: can multiple server work at same port (8089) ? NO 
+/**
+ * ERROR: 
+ * Error: listen EADDRINUSE: address already in use :::8089
+ * sudo lsof -i :3000
+ * kill -9 <PID>
+ * 
+ * 
+ * LINK: https://stackoverflow.com/questions/4075287/node-express-eaddrinuse-address-already-in-use-how-can-i-stop-the-process?newreg=8be80e1081074f61b72f94c744f249b0
+ */
